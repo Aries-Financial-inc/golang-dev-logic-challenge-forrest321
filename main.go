bfa0eae0cdaacb06e34c01784bdec41922d069fe
Forrest Ledbetter <forrest321@gmail.com>
2024-06-13 20:41:54 -0500

    server setup w/graceful shutdown, router

--- main.go
+++ main.go
@@ -1,17 +1,53 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	"errors"
 	"fmt"
+	"github.com/Aries-Financial-inc/golang-dev-logic-challenge-forrest321/routes"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/analyze", analyzeHandler)
+	// Create context that listens for the interrupt signal from the OS.
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	router := routes.SetupRouter()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
 
 	fmt.Println("Starting server on port 8080")
-	http.ListenAndServe(":8080", nil)
-}
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
 
-func analyzeHandler(w http.ResponseWriter, r *http.Request) {
-	// Your code here
+	// Listen for the interrupt signal.
+	<-ctx.Done()
+
+	// Restore default behavior on the interrupt signal and notify user of shutdown.
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
+
 }
